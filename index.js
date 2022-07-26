82b982ccafdf518cefb0e3aa60799d9b79e79e92
Aaron Everette <aeverette0120@gmail.com>
2022-07-26 18:32:11 -0400

    finished the bonus

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function tile(url,left, bottom, width, height) {
+    for(let h = 0; h < height; h++) {
+        for( let w = 0; w < width; w++) {
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+
 function newImage(url, left, bottom) {
     let object = document.createElement('img')
     object.src = url
@@ -16,7 +25,14 @@ function newItem(url, left, bottom) {
     })
 }
 
-newImage('assets/greaan-character.gif', 100, 100)
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/pine-tree.png', 450, 200)
@@ -24,5 +40,5 @@ newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
 newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
+newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
